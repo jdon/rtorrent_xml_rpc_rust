2e8aef072bfde672f09dff0534c535dc02117d71
Jonathan Donaldson <github@cdn.fail>
2020-03-21 01:16:18 +0000

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,18 @@ use std::net::TcpStream;
 
 pub fn make_request(
 	stream: &mut TcpStream,
-	xml: &String,
+	xml: &str,
 	buf: &mut Vec<u8>,
 ) -> std::io::Result<usize> {
 	let headers = generate_headers(xml);
 	let header_length = generate_header_length(&headers);
-	let request = generate_request(&header_length, &headers, xml).into_bytes();
+	let request = generate_request(header_length, &headers, xml).into_bytes();
 	let request_array: &[u8] = &request;
-	stream.write(request_array)?;
-	return stream.read_to_end(buf);
+	stream.write_all(request_array)?;
+	stream.read_to_end(buf)
 }
 
-fn generate_headers(xml: &String) -> Vec<String> {
+fn generate_headers(xml: &str) -> Vec<String> {
 	const NULL_CHAR: char = '\0';
 	let xml_length = xml.len();
 	let content_length = format!(
@@ -25,18 +25,18 @@ fn generate_headers(xml: &String) -> Vec<String> {
 	let scgi = format!("SCGI{null_char}1{null_char}", null_char = NULL_CHAR);
 	let array: Vec<String> = vec![content_length, scgi];
 
-	return array;
+	array
 }
 
-fn generate_header_length(headers: &Vec<String>) -> usize {
+fn generate_header_length(headers: &[String]) -> usize {
 	let mut length = 0;
 	for header in headers {
-		length = length + header.len();
+		length += header.len();
 	}
-	return length;
+	length
 }
 
-fn generate_request(header_length: &usize, headers: &Vec<String>, xml: &String) -> String {
+fn generate_request(header_length: usize, headers: &[String], xml: &str) -> String {
 	let b = headers.join(",");
 	format!("{}:{}{}", header_length, b, xml)
 }
